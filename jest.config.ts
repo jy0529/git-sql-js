d4b19cbd0f8f0414eba16074076176f186ba2632
Jy <1041207253@qq.com>
2023-06-18 22:08:11 +0800

    feat: add test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: 'ts-jest',
+	testEnvironment: 'node',
 	testMatch: ['<rootDir>/**/__tests__/**/*.spec.ts'],
 	testPathIgnorePatterns: ['/node_modules/'],
 	moduleFileExtensions: [
 		"ts",
 		"js",
 		"tsx"
-	]
+	],
+	collectCoverage: true,
+	collectCoverageFrom: [
+		"./source/**"
+	],
+	coverageThreshold: {
+		global: {
+			lines: 90
+		}
+	}
 };
